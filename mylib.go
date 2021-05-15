ebdfbfbfd198ded122af04acd841ccc5d7114c7e
GOZU Kenichiro <gozuk16@gmail.com>
2021-05-15 11:11:46 +0900

    hostとdisk情報を追加

--- mylib.go
+++ mylib.go
@@ -1,10 +1,22 @@
 package mylib
 
 import (
+	"encoding/json"
 	"fmt"
 
+	"github.com/shirou/gopsutil/v3/disk"
+	"github.com/shirou/gopsutil/v3/host"
 	"github.com/shirou/gopsutil/v3/mem"
 )
+
+func Host() []byte {
+	v, _ := host.Info()
+
+	// convert to JSON. String() is also implemented
+	fmt.Println(v)
+
+	return []byte(v.String())
+}
 
 func Mem() []byte {
 	v, _ := mem.VirtualMemory()
@@ -16,4 +28,17 @@ func Mem() []byte {
 	fmt.Println(v)
 
 	return []byte(v.String())
+}
+
+func Disk() []byte {
+	v, _ := disk.Partitions(true)
+
+	// convert to JSON. String() is also implemented
+	fmt.Println(v)
+
+	d, _ := json.Marshal(v)
+	//for i := 0; i < len(v); i++ {
+	//	d[i] = []byte(v[i].String())
+	//}
+	return d
 }
