cefb71891e8f271afc3f32ecab94b9769ad5146f
GOZU Kenichiro <gozuk16@gmail.com>
2021-05-21 11:52:00 +0900

    メモリを見やすく整形。CPU、LoadAverageを追加

--- mylib.go
+++ mylib.go
@@ -5,9 +5,12 @@ import (
 	"fmt"
 	"math"
 	"strconv"
+	"time"
 
+	"github.com/shirou/gopsutil/v3/cpu"
 	"github.com/shirou/gopsutil/v3/disk"
 	"github.com/shirou/gopsutil/v3/host"
+	"github.com/shirou/gopsutil/v3/load"
 	"github.com/shirou/gopsutil/v3/mem"
 
 	"github.com/inhies/go-bytesize"
@@ -32,16 +35,65 @@ func Host() []byte {
 	return []byte(v.String())
 }
 
+func Cpu() []byte {
+	c, _ := cpu.Percent(time.Millisecond*200, false)
+	core, _ := cpu.Percent(time.Millisecond*200, true)
+	//fmt.Printf("%f%%\n", c)
+
+	var cpu map[string]interface{}
+	total := strconv.FormatFloat(c[0], 'f', 0, 64) + "%"
+	var p = []string{}
+	for _, v := range core {
+		p = append(p, strconv.FormatFloat(v, 'f', 0, 64)+"%")
+	}
+	cpu = map[string]interface{}{
+		"total":  total,
+		"percpu": p,
+	}
+	j, _ := json.Marshal(cpu)
+
+	return j
+}
+
+func Load() []byte {
+	l, _ := load.Avg()
+	//fmt.Printf("%f\n", l)
+
+	var loadAvg map[string]interface{}
+	loadAvg = map[string]interface{}{
+		"load1":  strconv.FormatFloat(l.Load1, 'f', 2, 64),
+		"load5":  strconv.FormatFloat(l.Load5, 'f', 2, 64),
+		"load15": strconv.FormatFloat(l.Load15, 'f', 2, 64),
+	}
+	j, _ := json.Marshal(loadAvg)
+
+	return j
+}
+
 func Mem() []byte {
 	v, _ := mem.VirtualMemory()
 
 	// almost every return value is a struct
-	fmt.Printf("Total: %v, Free:%v, UsedPercent:%f%%\n", v.Total, v.Free, v.UsedPercent)
+	//fmt.Printf("Total: %v, Free:%v, UsedPercent:%f%%\n", v.Total, v.Free, v.UsedPercent)
 
 	// convert to JSON. String() is also implemented
-	fmt.Println(v)
+	//fmt.Println(v)
 
-	return []byte(v.String())
+	var mem map[string]interface{}
+	total := bytesize.New(float64(v.Total))
+	available := bytesize.New(float64(v.Available))
+	used := bytesize.New(float64(v.Used))
+	bytesize.Format = "%.1f "
+	usedPercent := math.Round(v.UsedPercent*10) / 10
+	//fmt.Printf("%v %v %v", total, free, used)
+	mem = map[string]interface{}{
+		"total":       total.String(),
+		"available":   available.String(),
+		"used":        used.String(),
+		"usedPercent": strconv.FormatFloat(usedPercent, 'f', -1, 64) + "%",
+	}
+	j, _ := json.Marshal(mem)
+	return j
 }
 
 func Disk() []byte {
