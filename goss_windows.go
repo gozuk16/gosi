d4558e6faf9e66aa04528678cc45c90e2b2a914a
GOZU Kenichiro <gozuk16@gmail.com>
2021-08-28 16:27:24 +0900

    Windows用のCPU温度取得追加

---
+++ goss_windows.go
@@ -0,0 +1,23 @@
+package goss
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/shirou/gopsutil/v3/host"
+)
+
+func getTemperatures() (string, error) {
+	t, err := host.SensorsTemperatures()
+	if err != nil {
+		fmt.Println(err.Error())
+		return "", err
+	}
+
+	for _, v := range t {
+		if v.SensorKey == "ACPI\\ThermalZone\\THM0_0" {
+			return strconv.FormatFloat(v.Temperature, 'f', -1, 64) + "℃", nil
+		}
+	}
+	return "", nil
+}
